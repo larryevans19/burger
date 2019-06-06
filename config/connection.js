e18a4776e5167cf6129be44f1bfe58145702df10
L-Train <train19@gmail.com>
2019-06-05 22:39:25 -0400

    v1.1 Burger (JawsDB Connection Bug)

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,22 @@
 // Setup code to connect Node to MySQL.
 const mysql = require("mysql");
 
-const connection;
+var connection;
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  host: "192.168.99.100",
-  port: 3306,
-  user: "root",
-  password: "docker",
-  database: "burgers_db"
-});
+    host: "192.168.99.100",
+    port: 3306,
+    user: "root",
+    password: "docker",
+    database: "burgers_db"
+  });
+};
 
 // Make the connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("Error Connecting: " + err.stack);
     return;
