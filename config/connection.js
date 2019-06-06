4dfd28492e76c3647fc5031a5e29dd1daf6675bb
L-Train <train19@gmail.com>
2019-06-05 21:51:14 -0400

    v1.0 Burger Main

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+// Setup code to connect Node to MySQL.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "192.168.99.100",
+  port: 3306,
+  user: "root",
+  password: "docker",
+  database: "burgers_db"
+});
+
+// Make the connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("Error Connecting: " + err.stack);
+    return;
+  }
+  console.log("Connected as ID " + connection.threadId);
+});
+
+// Export connection for the ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name)
+VALUES 
+("Cookout Big Double Out West Style"),
+("McDonald's Big N' Tasty"),
+("Burger King Whopper"),
+("Wendy's Son of Baconator")
